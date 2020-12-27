e74db6af51ed814bcd788c509f75ff34713d930c
Elton Law <eltonlaw296@gmail.com>
2020-12-27 01:23:51 -0500

    add proper testing of tokenization

--- sendak.hpp
+++ sendak.hpp
@@ -1,22 +1,30 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <ctype.h>
 
 namespace sendak {
+
+template<typename T>
+bool is_equal(std::vector<T> const &v1, std::vector<T> const &v2)
+{
+    return (v1.size() == v2.size() &&
+            std::equal(v1.begin(), v1.end(), v2.begin()));
+}
+
 namespace lisp {
 
 /* create a list of tokens */
-std::vector<std::string> tokenizer(std::string fp) {
-    std::ifstream is(fp);
-    char c;
+std::vector<std::string> tokenize(std::string source_code) {
     std::vector<std::string> tokens;
     std::string str;
 
     std::string specials {"+-()\"'"};
-    while (is.get(c)) {
+    for (char const &c: source_code) {
         // To handle arbitrarily long names, if previous char was an
         // alphanumeric and current one isn't then the symbol has
         // terminated and can be pushed to the tokens vector
@@ -39,12 +47,20 @@ std::vector<std::string> tokenizer(std::string fp) {
             tokens.push_back(std::string {c});
         }
     }
-    is.close();
     return tokens;
 }
 
-void Parser::read(std::string fp) {
-    auto tokens = tokenizer(fp);
+std::vector<std::string> tokenize_file(std::string fp) {
+    std::ifstream is(fp);
+    std::stringstream buffer;
+    buffer << is.rdbuf();
+    is.close();
+
+    return tokenize(buffer.str());
+}
+
+void read_file(std::string fp) {
+    auto tokens = tokenize(fp);
 
     for (auto t: tokens) {
         std::cout << t << " ";
